f5cd660f08fd2c7ce64ab90f97217a61e88175ab
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-06-11 20:36:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,28 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-puts "prints a blank board when the board array is empty"
+#puts "prints a blank board when the board array is empty"
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
 
-puts "prints a board with an X in the center position"
-board = [" "," "," "," ","X"," "," "," "," "]
-display_board(board)
 
-puts "prints a board with O in the top left"
-board = ["O"," "," "," "," "," "," "," "," "]
-display_board(board)
 
-puts "prints a board with an X in the center and an O in the top left"
-board = ["O"," "," "," ","X"," "," "," "," "]
-display_board(board)
 
-puts "prints a board with X winning via the top row"
-board = ["X","X","X"," "," "," "," "," "," "]
-display_board(board)
 
-puts "prints a board with O winning via the bottom row"
-board = [" "," "," "," "," "," ","O","O","O"]
 display_board(board)
 
 puts "prints a board with X winning in a top left to bottom right diagonal"
@@ -42,3 +28,46 @@ display_board(board)
 puts "prints arbitrary arrangements of the board"
 board = ["O","O","X","X","O","O","X","X","X"]
 display_board(board)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#puts "prints a board with an X in the center position"
+#board = [" "," "," "," ","X"," "," "," "," "]
+#display_board(board)
+
+#puts "prints a board with O in the top left"
+#board = ["O"," "," "," "," "," "," "," "," "]
+#display_board(board)
+
+#puts "prints a board with an X in the center and an O in the top left"
+#board = ["O"," "," "," ","X"," "," "," "," "]
+#display_board(board)
+
+#puts "prints a board with X winning via the top row"
+#board = ["X","X","X"," "," "," "," "," "," "]
+#display_board(board)
+
+#puts "prints a board with O winning via the bottom row"
+#board = [" "," "," "," "," "," ","O","O","O"]
